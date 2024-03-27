858385ee3b8db4184a98e50bff4e50710a39f142
김주환 <jh0416@haegin.kr>
2024-03-27 18:57:39 +0900

    - 프로젝트 구성 (라이브러리, 콘솔 앱) - ClosedXml 라이브러리 추가 - 엑셀파일로부터 워크시트 내 유효한 셀들을 조회하고 DataTable 타입으로 변환

---
+++ ExcelDataSerializerConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExcelDataSerializer;
+
+namespace ExcelDataSerializerConsole;
+
+internal abstract class Program
+{
+    static void Main()
+    {
+        var files = Directory.GetFiles("Excel");
+        var loader = new Loader();
+        foreach (var file in files)
+        {
+            var fullPath = Path.GetFullPath(file);
+            loader.LoadXls(fullPath); 
+        }
+    } 
+}
