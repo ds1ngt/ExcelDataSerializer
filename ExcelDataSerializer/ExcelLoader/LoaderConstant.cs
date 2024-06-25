c1ce41c371ff880f77c241b762e12154a9d5f757
김주환 <jh0416@haegin.kr>
2024-06-25 19:00:03 +0900

    버그 수정

--- ExcelDataSerializer/ExcelLoader/LoaderConstant.cs
+++ ExcelDataSerializer/ExcelLoader/LoaderConstant.cs
@@ -4,5 +4,5 @@ public abstract class LoaderConstant
 {
     public const int HEADER_NAME_ROW = 1;
     public const int SCHEMA_ROW = 2;
-    public const int DATA_BEGIN_ROW = 3;
+    public const int DATA_BEGIN_ROW = 4;
 }
